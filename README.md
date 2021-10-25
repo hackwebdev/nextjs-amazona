d1beb1124ac4229d6f175dd63bb4bbcc32e260aa
hackwebdev <hackwebdev@gmail.com>
2021-10-25 22:39:24 +0800

    add style to app

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ npm install @material-ui/core
 rfc - react functional component
 nih - import Head
 nuh - use Head
+
+Destructure
+const { main, navbar, footer } = classes
+instead of
+
+- classes.main
+- classes.navbar
--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,22 @@
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
 import Head from 'next/head'
+import useStyles from '../utils/styles'
 
 export default function Layout({ children }) {
+  const classes = useStyles()
+  const { main, navbar, footer } = classes
   return (
     <div>
       <Head>
         <title>Next Amazona</title>
       </Head>
-      <AppBar position='static'>
+      <AppBar position='static' className={navbar}>
         <Toolbar>
           <Typography>amazona</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>
+      <Container className={main}>{children}</Container>
+      <footer className={footer}>
         <Typography>All rights reserve . Next Amazona</Typography>
       </footer>
     </div>

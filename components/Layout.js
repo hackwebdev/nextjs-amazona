4045ea1b03eecbdfbe7aa6f562e3bebc154566be
hackwebdev <hackwebdev@gmail.com>
2021-10-26 00:28:30 +0800

    product details page

--- components/Layout.js
+++ components/Layout.js
@@ -3,13 +3,14 @@ import { AppBar, Toolbar, Typography, Container, Link } from '@material-ui/core'
 import useStyles from '../utils/styles'
 import Head from 'next/head'
 
-export default function Layout({ children }) {
+export default function Layout({ title, description, children }) {
   const classes = useStyles()
 
   return (
     <div>
       <Head>
-        <title>Next Amazona</title>
+        <title>{title ? `${title} - Next Amazona` : 'Next Amazona'}</title>
+        {description && <meta name='description' content={description}></meta>}
       </Head>
       <AppBar position='static' className={classes.navbar}>
         <Toolbar>
